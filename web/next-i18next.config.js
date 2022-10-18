effecdff5c25ed3243b01fcb9431b9e681b06c8c
Santi Aguirre <saguirrews@gmail.com>
2022-10-18 15:58:12 -0300

    Update next-i18next.config.js

--- web/next-i18next.config.js
+++ web/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en'],
+    localePath: path.resolve('./public/locales'),
     serializeConfig: false,
     interpolation: {
       format: (value, format, lng) => {
